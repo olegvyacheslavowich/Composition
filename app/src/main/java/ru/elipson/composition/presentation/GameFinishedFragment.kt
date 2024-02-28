37c0006dd705ecfec4e8178191cd30864dda03ea
karpenko_ov <20_ok_13@mail.ru>
2024-02-29 01:40:11 +0500

    layout added

---
+++ app/src/main/java/ru/elipson/composition/presentation/GameFinishedFragment.kt
@@ -0,0 +1,17 @@
+package ru.elipson.composition.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.elipson.composition.R
+
+class GameFinishedFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game_finished, container, false)
+    }
+}
